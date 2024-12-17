7463d678a8a4574c6a1eb88a63f368b4a0002311
amirkakon <amir.kakon@gmail.com>
2024-12-17 18:08:06 +0200

    deployed dev and prod

--- functions/main.py
+++ functions/main.py
@@ -16,6 +16,7 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 app.config.from_object(config.Config)
 
+# Register the routes
 app.register_blueprint(github_routes, url_prefix="/api/github")
 
 @app.route("/")
@@ -26,7 +27,7 @@ def show_endpoints():
     for rule in app.url_map.iter_rules():
         # Skip static files and internal Flask routes
         if rule.endpoint != 'static' and not rule.endpoint.startswith('flask') and rule.rule != '/':
-            # Get the prefix (e.g., api/github, api/test)
+            # Get the prefix (e.g., api/github, api/config)
             parts = rule.rule.split("/")
             prefix = parts[2]
 
@@ -46,13 +47,33 @@ def dev():
     logger.debug(f"Dev endpoint called, You are in env: {env}")
     return jsonify({"ENV": env, "PORT": app.config.get('PORT')})
 
+
+# Create a function for the development environment
+@https_fn.on_request(max_instances=1)
+def jarvis_cloud_dev(req: https_fn.Request) -> https_fn.Response:
+    env = os.getenv("FLASK_ENV")
+    if env == "development":
+        logger.info("Development environment detected.")
+        with app.request_context(req.environ):
+            return app.full_dispatch_request()
+    else:
+        logger.warning("Development function called in non-development environment.")
+        return jsonify({"error": "Not in development environment."}), 400
+
+
+# Create a function for the production environment
+@https_fn.on_request(max_instances=1)
+def jarvis_cloud_prod(req: https_fn.Request) -> https_fn.Response:
+    env = os.getenv("FLASK_ENV")
+    if env == "production":
+        logger.info("Production environment detected.")
+        with app.request_context(req.environ):
+            return app.full_dispatch_request()
+    else:
+        logger.warning("Production function called in non-production environment.")
+        return jsonify({"error": "Not in production environment."}), 400
+
+
 if __name__ == "__main__":
-    # Get port from config and start the app
     port = app.config.get('PORT', 3000)
     app.run(debug=app.config.get('DEBUG', True), host="0.0.0.0", port=port)
-
-
-@https_fn.on_request(max_instances=1)
-def jarvis_cloud(req: https_fn.Request) -> https_fn.Response:
-    with app.request_context(req.environ):
-        return app.full_dispatch_request()
